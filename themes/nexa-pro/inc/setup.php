c9696f1af0073b8d76e2c14fd2cfd96b96fe4ef6
Manish Chawla <om.manishchawla@gmail.com>
2026-07-09 09:13:16 -0700

    feat: add Nexa Pro design system CSS foundation

--- themes/nexa-pro/inc/setup.php
+++ themes/nexa-pro/inc/setup.php
@@ -24,7 +24,7 @@ function nexa_pro_setup() {
 	add_theme_support( 'wp-block-styles' );
 	add_theme_support( 'align-wide' );
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'assets/css/theme.css' );
+	add_editor_style( 'assets/css/editor.css' );
 
 	add_theme_support(
 		'html5',
